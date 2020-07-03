2765d50554a5d2aac36bbe10b9e2fcdd0656d2ca
Marcos Alves Trindade Junior <marcosatjr2000@gmail.com>
2020-07-02 22:05:27 -0400

    cadastro de users implementado com validações

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Controller,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  @UsePipes(ValidationPipe)
+  async signUp(@Body() authCredentials: AuthCredentialsDto): Promise<void> {
+    this.authService.singnUp(authCredentials);
+  }
+}
